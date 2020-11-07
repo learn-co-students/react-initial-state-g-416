f38d968e3fe37e52c7052439b00953745d8b4fb7
Tracy Ngo <tngo1206@gmail.com>
2020-11-06 19:47:21 -0800

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -2,20 +2,29 @@
 
 import React from 'react';
 
+// class Address extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       fullAddress: `${props.street}, ${props.city}`
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="address">
+//         {this.state.fullAddress}
+//       </div>
+//     );
+//   }
+// }
+
+
 class Address extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fullAddress: `${props.street}, ${props.city}`
-    }
-  }
-
   render() {
     return (
-      <div className="address">
-        {this.state.fullAddress}
-      </div>
+      <div className="address">{this.props.street}, {this.props.city}</div>
     );
   }
 }
