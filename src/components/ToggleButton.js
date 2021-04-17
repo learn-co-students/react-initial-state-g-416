dc396131e7339443cba838871fba32e10f3d9f13
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-17 08:55:18 -0400

    Done.

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,17 @@
 // Component Code Goes Here
+import React, { Component } from 'react'
+
+export default class ToggleButton extends Component {
+    state = 
+    {
+        isEnabled: false
+    }
+
+    render() {
+        return (
+            <button className="toggle-button">
+                I am toggled {this.state.isEnabled ? "on" : "off"}
+            </button>
+        )
+    }
+}
